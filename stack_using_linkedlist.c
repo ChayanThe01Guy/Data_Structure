216b28eb592d5f6bf88412930ea48cbea91990b2
Chayan Roy <83724456+ChayanThe01Guy@users.noreply.github.com>
2021-09-06 14:24:58 +0530

    Add files via upload

---
+++ stack_using_linkedlist.c
@@ -0,0 +1,83 @@
+#include <stdio.h>  
+#include <stdlib.h>  
+
+void push(int);  
+void pop();  
+void display();  
+
+struct node {
+	int val;  
+	struct node *next;  
+};
+struct node *head;  
+
+int count = 0;
+
+void main () {
+	display();
+	push(50);
+	push(40);
+	push(30);
+	push(20);
+	push(10);
+	display();
+	pop();
+	pop();
+	pop();
+	display();
+}  
+
+void push (int x) {
+
+	struct node *ptr = (struct node*) malloc (sizeof (struct node));
+	
+	if(ptr == NULL) {  
+		printf("Stack space not create\n");   
+	}
+	else if(head == NULL) {
+		ptr -> val = x;  
+		ptr -> next = NULL;  
+		head = ptr;  
+	}
+	else {  
+		ptr -> val = x;  
+		ptr -> next = head;  
+		head = ptr;
+	}
+}
+  
+void pop () {  
+    int item;
+    struct node *ptr; 
+
+	if (head == NULL) {  
+		printf ("\n\nStack Is Empty\n");  
+	}  
+	else {  
+		item = head -> val;  
+		ptr = head;  
+		head = head -> next;  
+		free (ptr);
+		printf("POPPED item is = %d\n", item);
+	}
+	printf ("\n");
+}
+
+
+void display () {
+	
+    struct node *ptr; 
+	
+    ptr = head;  
+    if (ptr == NULL) {  
+        printf ("Stack is empty\n");  
+    }  
+    else {  
+        printf ("Printing Stack elements \n");  
+        while (ptr != NULL) {  
+            printf ("%d\n",ptr -> val);  
+            ptr = ptr -> next;  
+        }  
+    }
+    printf ("\n");
+}
